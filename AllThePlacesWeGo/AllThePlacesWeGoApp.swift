71c63f2391921a61a3f95564991c2671e8f03984
Annalie <annaliakruseman@gmail.com>
2023-10-17 11:44:12 -0700

    Initial Commit

---
+++ AllThePlacesWeGo/AllThePlacesWeGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllThePlacesWeGoApp.swift
+//  AllThePlacesWeGo
+//
+//  Created by Annalie Kruseman on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct AllThePlacesWeGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
